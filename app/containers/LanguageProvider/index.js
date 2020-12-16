26edfb88232d79963061b7439057f90877ba04c3
marzavec <admin@marzavec.com>
2020-12-16 11:14:25 -0800

    lint & bug fix

--- app/containers/LanguageProvider/index.js
+++ app/containers/LanguageProvider/index.js
@@ -6,25 +6,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { IntlProvider, ReactIntlErrorCode } from 'react-intl';
+import { IntlProvider } from 'react-intl';
 
 import { makeSelectLocale } from './selectors';
 
 /**
  * This error handling function is directly related to
  * https://github.com/hack-chat/hackchat-client/pull/27
- * 
  * Relavent issue at:
  * https://github.com/hack-chat/hackchat-client/issues/30
  */
 const checkError = (e) => {
-  if (e.code = ReactIntlErrorCode.MISSING_DATA) {
-    if (e.toString().indexOf('Missing locale data for locale: "cn"') !== -1)
-      return;
+  if (e.toString().indexOf('Missing locale data for locale: "cn"') !== -1) {
+    return;
   }
 
-  console.error(e);
-}
+  console.error(e); // eslint-disable-line no-console
+};
 
 export function LanguageProvider(props) {
   return (
